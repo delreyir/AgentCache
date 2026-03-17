be869a04d7a252744ca9c453d010b14077153033
delrey <116382547+delreyir@users.noreply.github.com>
2026-03-17 16:38:39 +0000

    fix: remove legacy petra plugin

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AptosWalletAdapterProvider } from '@aptos-labs/wallet-adapter-react';
-import { PetraWallet } from 'petra-plugin-wallet-adapter';
 import App from './App.tsx';
 
-// Hna fin kan-goulou l-app dyalna t-kheddem Petra
-const wallets = [new PetraWallet()];
+// 🚀 L-7ell: 7iydna 'petra-plugin-wallet-adapter' b merra!
+// L-Aptos Wallet Adapter jdid kay-chouf Petra Wallet w ay wallet jdida (AIP-62 Standard) rasso bla plugins.
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* Hada huwa l-Provider li kan khass */}
-    <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
+    {/* Khlli Provider bla plugins, ghadi y-detecti l-extension rasso */}
+    <AptosWalletAdapterProvider autoConnect={true}>
       <App />
     </AptosWalletAdapterProvider>
   </React.StrictMode>
